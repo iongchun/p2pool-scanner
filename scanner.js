90885a2765fe78047a2a225c0a937cda11608d5f
Ang Iongchun <angiongchun@gmail.com>
2014-03-19 08:26:41 +0800

    fix use of uninitialized var

--- scanner.js
+++ scanner.js
@@ -205,7 +205,7 @@ function Scanner(options) {
         // console.log("P2POOL DIGESTING:",info.ip);
 
         digest_local_stats(info, function(err, stats){
-            if(!err && info.stats.protocol_version >= 1300) {
+            if(!err && stats.protocol_version >= 1300) {
                 // Exclude nodes lacking protocol_version or older than 1300
                 info.stats = stats;
                 info.fee   = stats.fee;
